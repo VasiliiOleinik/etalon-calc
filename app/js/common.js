1078b145b3644b4880ac66bfc9d44be37d896d99
Vasilii <goooneoooogle@gmail.com>
2019-11-07 10:38:31 +0200

    Add adaptive

---
+++ app/js/common.js
@@ -0,0 +1,5 @@
+$(function() {
+	if(screen.width < 992) {
+		$(".audio-player").insertAfter(".second-section__left-top");
+	}
+});
--- app/js/player.js
+++ app/js/player.js
@@ -1,23 +1,89 @@
-$(
-	function(){
-	var aud = $('audio')[0];
-	$('.play-pause').on('click', function(){
-	if (aud.paused) {
-	  aud.play();
-	  $('.play-pause').removeClass('icon-play');
-	  $('.play-pause').addClass('icon-stop');
+function calculateTotalValue(length) {
+	var minutes = Math.floor(length / 60),
+	  seconds_int = length - minutes * 60,
+	  seconds_str = seconds_int.toString(),
+	  seconds = seconds_str.substr(0, 2),
+	  time = minutes + ':' + seconds;
+  
+	return time;
+  }
+  
+  function calculateCurrentValue(currentTime) {
+	var current_hour = parseInt(currentTime / 3600) % 24,
+	  current_minute = parseInt(currentTime / 60) % 60,
+	  current_seconds_long = currentTime % 60,
+	  current_seconds = current_seconds_long.toFixed(),
+	  current_time = (current_minute < 10 ? "0" + current_minute : current_minute) + ":" + (current_seconds < 10 ? "0" + current_seconds : current_seconds);
+  
+	return current_time;
+  }
+  
+  function initProgressBar() {
+	var player = document.getElementById('player');
+	var length = player.duration;
+	var current_time = player.currentTime;
+  
+	// calculate total length of value
+	var totalLength = calculateTotalValue(length)
+	jQuery(".end-time").html(totalLength);
+  
+	// calculate current value time
+	var currentTime = calculateCurrentValue(current_time);
+	jQuery(".start-time").html(currentTime);
+  
+	var progressbar = document.getElementById('seekObj');
+	progressbar.value = (player.currentTime / player.duration);
+	progressbar.addEventListener("click", seek);
+  
+	if (player.currentTime == player.duration) {
+	  $('#play-btn').removeClass('pause');
 	}
-	else {
-	  aud.pause();
-	  $('.play-pause').removeClass('icon-stop');
-	  $('.play-pause').addClass('icon-play');
+  
+	function seek(evt) {
+	  var percent = evt.offsetX / this.offsetWidth;
+	  player.currentTime = percent * player.duration;
+	  progressbar.value = percent / 100;
 	}
-	
-  })
-	$('.next').on('click', function(){
-	aud.src = 'another audio source';
-  })
-	aud.ontimeupdate = function(){
-	  $('.progress').css({'width': aud.currentTime / aud.duration * 100 + '%', "height": "2px", "background":"red"})
+  };
+  
+  function initPlayers(num) {
+	// pass num in if there are multiple audio players e.g 'player' + i
+  
+	for (var i = 0; i < num; i++) {
+	  (function() {
+  
+		// Variables
+		// ----------------------------------------------------------
+		// audio embed object
+		var playerContainer = document.getElementById('player-container'),
+		  player = document.getElementById('player'),
+		  isPlaying = false,
+		  playBtn = document.getElementById('play-btn');
+  
+		// Controls Listeners
+		// ----------------------------------------------------------
+		if (playBtn != null) {
+		  playBtn.addEventListener('click', function() {
+			togglePlay()
+		  });
+		}
+  
+		// Controls & Sounds Methods
+		// ----------------------------------------------------------
+		function togglePlay() {
+		  if (player.paused === false) {
+			player.pause();
+			isPlaying = false;
+			$('#play-btn').removeClass('pause');
+  
+		  } else {
+			player.play();
+			$('#play-btn').addClass('pause');
+			isPlaying = true;
+		  }
+		}
+	  }());
 	}
-  })
+  }
+  
+  initPlayers(jQuery('#player-container').length);
